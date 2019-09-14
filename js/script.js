c95be20d979200ecb51ef0f22fe5e97294ff4772
frejonb <fernandorejonbarrera@wlnss.com>
2019-09-14 10:50:51 +0200

    add js script

---
+++ js/script.js
@@ -0,0 +1,98 @@
+const COLORS = {
+  RED:      '#FD5061',
+  YELLOW:   '#FFCEA5',
+  BLACK:    '#29363B',
+  WHITE:    'white',
+  VINOUS:   '#A50710'
+}
+
+const burst1Config = {
+  left: 0, top: 0,
+  count:          8,
+  radius:         { 50: 150 },
+  children: {
+    shape:        'line',
+    stroke:       [ 'white', '#FFE217', '#FC46AD', '#D0D202', '#B8E986', '#D0D202' ],
+    scale:        1,
+    scaleX:       { 1 : 0 },
+    // pathScale:    'rand(.5, 1.25)',
+    degreeShift:  'rand(-90, 90)',
+    radius:       'rand(20, 40)',
+    // duration:     200,
+    delay:        'rand(0, 150)',
+    isForce3d:    true
+  }
+};
+
+const largeBurstConfig = {
+  left: 0, top: 0,
+  count:          4,
+  radius:         0,
+  angle:         45,
+  radius:        { 0: 450  },
+  children: {
+    shape:        'line',
+    stroke:       '#4ACAD9',
+    scale:        1,
+    scaleX:       { 1 : 0 },
+    radius:       100,
+    duration:     450,
+    isForce3d:    true,
+    easing:       'cubic.inout'
+  }
+};
+
+const CIRCLE_OPTS = {
+  left: 0, top: 0,
+  scale:      { 0: 1 },
+}
+
+const largeCircleConfig = {
+  ...CIRCLE_OPTS,
+  fill:       'none',
+  stroke:     'white',
+  strokeWidth: 4,
+  opacity:    { .25 : 0 },
+  radius:     250,
+  duration:   600,
+};
+
+const smallCircleConfig = {
+  ...CIRCLE_OPTS,
+  fill:       'white',
+  opacity:    { .5 : 0 },
+  radius:     30,
+};
+  
+async function createBurst(x, y) {
+  new mojs.Burst(burst1Config)
+    .tune({ x: x, y: y })
+    .generate()
+    .replay();
+  
+  new mojs.Burst(largeBurstConfig)
+    .tune({ x: x, y: y })
+    .replay();
+  
+  new mojs.Shape(largeCircleConfig)
+    .tune({ x: x, y: y })
+    .replay();
+  
+  new mojs.Shape(smallCircleConfig)
+    .tune({ x: x, y: y })
+    .replay();
+}
+
+async function handleBurst(keyEvent) {
+  const maxWidth = document.documentElement.clientWidth
+  const maxHeight = document.documentElement.clientHeight
+
+  x = Math.floor((Math.random() * maxWidth) + 1);
+  y = Math.floor((Math.random() * maxHeight) + 1);
+
+  await createBurst(x, y)
+  
+}
+
+document.addEventListener("keydown", () =>
+  handleBurst())
