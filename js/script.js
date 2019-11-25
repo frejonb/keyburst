a077281ab98c83647214f94a6a336762a67cefb6
frejonb <fernandorejonbarrera@wlnss.com>
2019-11-25 20:13:01 +0100

    asd

--- js/script.js
+++ js/script.js
@@ -124,67 +124,19 @@ var haveEvents = 'ongamepadconnected' in window;
 var controllers = {};
 
 function connecthandler(e) {
-  addgamepad(e.gamepad);
+  controllers[e.gamepad.index] = e.gamepad;
+  startAnimation()
 }
 
-function addgamepad(gamepad) {
-  controllers[gamepad.index] = gamepad;
-
-  var d = document.createElement("div");
-  d.setAttribute("id", "controller" + gamepad.index);
-
-  var t = document.createElement("h1");
-  t.appendChild(document.createTextNode("gamepad: " + gamepad.id));
-  d.appendChild(t);
-
-  var b = document.createElement("div");
-  b.className = "buttons";
-  for (var i = 0; i < gamepad.buttons.length; i++) {
-    var e = document.createElement("span");
-    e.className = "button";
-    //e.id = "b" + i;
-    e.innerHTML = i;
-    b.appendChild(e);
-  }
-
-  d.appendChild(b);
-
-  var a = document.createElement("div");
-  a.className = "axes";
-
-  for (var i = 0; i < gamepad.axes.length; i++) {
-    var p = document.createElement("progress");
-    p.className = "axis";
-    //p.id = "a" + i;
-    p.setAttribute("max", "2");
-    p.setAttribute("value", "1");
-    p.innerHTML = i;
-    a.appendChild(p);
-  }
-
-  d.appendChild(a);
-
-  // See https://github.com/luser/gamepadtest/blob/master/index.html
-  var start = document.getElementById("start");
-  if (start) {
-    start.style.display = "none";
-  }
-
-  document.body.appendChild(d);
-  requestAnimationFrame(updateStatus);
+function startAnimation() {
+  requestAnimationFrame(frameLoop);
 }
 
 function disconnecthandler(e) {
-  removegamepad(e.gamepad);
+  delete controllers[e.gamepad.index];
 }
 
-function removegamepad(gamepad) {
-  var d = document.getElementById("controller" + gamepad.index);
-  document.body.removeChild(d);
-  delete controllers[gamepad.index];
-}
-
-function updateStatus() {
+function frameLoop() {
   if (!haveEvents) {
     scangamepads();
   }
@@ -194,11 +146,8 @@ function updateStatus() {
 
   for (j in controllers) {
     var controller = controllers[j];
-    var d = document.getElementById("controller" + j);
-    var buttons = d.getElementsByClassName("button");
 
     for (i = 0; i < controller.buttons.length; i++) {
-      var b = buttons[i];
       var val = controller.buttons[i];
       var pressed = val == 1.0;
       if (typeof(val) == "object") {
@@ -206,26 +155,17 @@ function updateStatus() {
         val = val.value;
       }
 
-      var pct = Math.round(val * 100) + "%";
-      b.style.backgroundSize = pct + " " + pct;
-
       if (pressed) {
-        b.className = "button pressed";
         handleBurstEvent()
-      } else {
-        b.className = "button";
       }
     }
 
-    var axes = d.getElementsByClassName("axis");
-    for (i = 0; i < controller.axes.length; i++) {
-      var a = axes[i];
-      a.innerHTML = i + ": " + controller.axes[i].toFixed(4);
-      a.setAttribute("value", controller.axes[i] + 1);
-    }
+    // for (i = 0; i < controller.axes.length; i++) {
+    //   controller.axes[i].toFixed(4);
+    // }
   }
 
-  requestAnimationFrame(updateStatus);
+  requestAnimationFrame(frameLoop);
 }
 
 function scangamepads() {
