9a9d2ba633a97b978bf76878a6579d7f0e075a19
frejonb <fernandorejonbarrera@wlnss.com>
2019-12-01 12:51:12 +0100

    add star animations

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,5 @@
+const RADIUS_STAR = 28;
+
 const COLORS = {
     RED: '#FD5061',
     YELLOW: '#FFCEA5',
@@ -64,12 +66,59 @@ const smallCircleConfig = {
     radius: 30,
 };
 
+class Star extends mojs.CustomShape {
+    getShape() {
+        return '<path d="M5.51132201,34.7776271 L33.703781,32.8220808 L44.4592855,6.74813038 C45.4370587,4.30369752 47.7185293,3 50,3 C52.2814707,3 54.5629413,4.30369752 55.5407145,6.74813038 L66.296219,32.8220808 L94.488678,34.7776271 C99.7034681,35.1035515 101.984939,41.7850013 97.910884,45.2072073 L75.9109883,63.1330483 L82.5924381,90.3477341 C83.407249,94.4217888 80.4739296,97.6810326 77.0517236,97.6810326 C76.0739505,97.6810326 74.9332151,97.3551083 73.955442,96.7032595 L49.8370378,81.8737002 L26.044558,96.7032595 C25.0667849,97.3551083 23.9260495,97.6810326 22.9482764,97.6810326 C19.3631082,97.6810326 16.2668266,94.4217888 17.4075619,90.3477341 L23.9260495,63.2960105 L2.08911601,45.2072073 C-1.98493875,41.7850013 0.296531918,35.1035515 5.51132201,34.7776271 Z" />';
+    }
+}
+mojs.addShape('star', Star);
+
 var canvas = document.querySelector("#canvas")
 var context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Animation {
+
+    async createStarBurst(x, y) {
+        await new Promise(resolve => {
+            const circleStar = new mojs.Shape({
+                left: 0, top: 0,
+                stroke: '#FF9C00',
+                strokeWidth: { [2 * RADIUS_STAR]: 0 },
+                fill: 'none',
+                scale: { 0: 1, easing: 'quad.out' },
+                radius: RADIUS_STAR,
+                duration: 450,
+                onComplete() {
+                    circleStar.el.parentNode.removeChild(circleStar.el);
+                },
+            }).tune({ x: x, y: y })
+                .generate()
+                .replay();;
+
+            const burstStar = new mojs.Burst({
+                left: 0, top: 0,
+                radius: { 6: RADIUS_STAR - 3 },
+                angle: 45,
+                children: {
+                    shape: 'star',
+                    radius: RADIUS_STAR / 2.2,
+                    fill: '#FD7932',
+                    degreeShift: 'stagger(0,-5)',
+                    duration: 700,
+                    delay: 200,
+                    easing: 'quad.out',
+                    // delay:        100,
+                },
+                onComplete() {
+                    burstStar.el.parentNode.removeChild(burstStar.el);
+                },
+            }).tune({ x: x, y: y })
+                .generate()
+                .replay();;
+        });
+    }
 
     async createBurst(x, y) {
         await new Promise(resolve => {
@@ -85,7 +134,7 @@ class Animation {
                 .tune({ x: x, y: y })
                 .generate()
                 .replay();
-    
+
             const largeBurst = new mojs.Burst({
                 ...largeBurstConfig,
                 onComplete() {
@@ -94,7 +143,7 @@ class Animation {
             })
                 .tune({ x: x, y: y })
                 .replay();
-    
+
             const largeCircle = new mojs.Shape({
                 ...largeCircleConfig,
                 onComplete() {
@@ -103,7 +152,7 @@ class Animation {
             })
                 .tune({ x: x, y: y })
                 .replay();
-    
+
             const smallCircle = new mojs.Shape({
                 ...smallCircleConfig,
                 onComplete() {
@@ -112,23 +161,23 @@ class Animation {
             })
                 .tune({ x: x, y: y })
                 .replay();
-    
+
         });
     }
 
-    async createRandomBurst() {
+    generateRandomCoordinateOnScreen() {
         const maxWidth = document.documentElement.clientWidth
         const maxHeight = document.documentElement.clientHeight
-        
+
         var x = Math.floor((Math.random() * maxWidth) + 1);
         var y = Math.floor((Math.random() * maxHeight) + 1);
-        await this.createBurst(x, y)
-    
+        return {x: x, y: y};
+
     }
 
     drawCrossHair(x, y) {
         context.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    
+
         context.beginPath();
         context.strokeStyle = "#FF6A6A";
         context.lineWidth = 1;
@@ -144,5 +193,23 @@ class Animation {
         context.setLineDash([]);
         context.arc(x, y, 20, 0, 2 * Math.PI, true);
         context.stroke();
+    }
+
+    drawStar(x, y) {
+        const star = new mojs.Shape({
+            left: 0, top: 0,
+            shape: 'star',
+            fill: '#FF9C00',
+            scale: { 0: 1 },
+            easing: 'elastic.out',
+            duration: 1600,
+            delay: 300,
+            radius: RADIUS_STAR / 2.35,
+            onComplete() {
+                star.el.parentNode.removeChild(star.el);
+            },
+        }).tune({ x: x, y: y })
+            .generate()
+            .replay();
     }
 }
